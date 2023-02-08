6b8b6a2859ae6452a5b25912057d51aa940e5252
Natalya <business_inbox@mail.ru>
2023-02-08 15:43:50 +0300

    Add Task_1

---
+++ Homework_8/Task_1/Program.cs
@@ -0,0 +1,54 @@
+﻿/* Задайте двумерный массив. Напишите программу, которая упорядочивает по убыванию элементы каждой строки двумерного массива.*/
+
+
+void FillMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+
+void PrintMatrix(int[,] matrix) 
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void SortToLower(int[,] matrix)   // Функция сортировки элементов в строке двумерного массива, по убыванию
+
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] matrix = new int[3, 5];
+FillMatrix(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
+SortToLower(matrix);
+PrintMatrix(matrix);
